dd7f0ddad8fd6f28e732ce5051823447feff4d32
Duduu_mendez <anoldbarnabas15@gmail.com>
2025-01-24 14:44:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'whatsappbot832@gmail.com' # Your email
-app.config['MAIL_PASSWORD'] = 'FANTASTICS' # Your email password
+app.config['MAIL_USERNAME'] = 'mendezduduu@gmail.com' # Your email
+app.config['MAIL_PASSWORD'] = 'FANTASTICSFOUR' # Your email password
 mail = Mail(app)
 
 @app.route('/')
